3d6b1109ecfd9bcee8024187286819eb77f50378
syovine <sergioyovine@gmail.com>
2018-11-28 23:45:13 -0300

    8th commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bees',
-    version='0.7',
+    version='0.1',
     packages=find_packages(exclude=[]),
     license='MIT',
     description='Bees',
